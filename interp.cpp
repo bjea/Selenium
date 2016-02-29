777feb45d0fe7042139ba66f340f6144d46e027f
Bettie Jea <bjea@ucsc.edu>
2016-02-29 06:31:59 -0800

    size_t

--- interp.cpp
+++ interp.cpp
@@ -144,10 +144,10 @@ shape_ptr interpreter::make_polygon (param begin, param end) {
    for (; begin != end; ++begin)
    {
       vertex v;
-      size_t* idx = 0;
-      v.xpos = stod(*begin, idx);
+      size_t idx = 0;
+      v.xpos = stod(*begin, &idx);
       begin++;
-      v.ypos = stod(*begin, idx);
+      v.ypos = stod(*begin, &idx);
       begin++;
       vertexList.push_back(v);
    }
